1f2459b1884b41206e3a7f371ee815a17e9255cd
John McDonald <96137405+jmcied@users.noreply.github.com>
2023-06-07 18:01:54 +0100

    Exercise 1 completed

--- src/components/exercises/01_staticComponent.js
+++ src/components/exercises/01_staticComponent.js
@@ -3,7 +3,31 @@ import "../../../node_modules/bootstrap/dist/css/bootstrap.css";
 
 const Demo = () => {
   return (
-      <h1>TODO</h1>
+    <div>
+      <h2>HDip in Computer Science</h2>
+    <table className="table table-bordered">
+  <thead>
+    <tr>
+      <th>Name</th>
+      <th>No lectures</th>
+      <th>No practicals</th>
+    </tr>
+  </thead>
+  <tbody >
+    <tr>
+      <td>DevOps</td>
+      <td>2</td>
+      <td>2</td>
+    </tr>
+    <tr>
+      <td>Full Stack Web Dev</td>
+      <td>3</td>
+      <td>2</td>
+    </tr>
+  </tbody >
+</table>
+</div>
+
   );
 };
 
